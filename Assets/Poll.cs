1f4e5a39a0bc1a7dfd70014a7c53409dd09065de
hemanth <hemuchiluka3@gmail.com>
2017-09-20 19:52:10 -0400

    polling rotation

---
+++ Assets/Poll.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Poll : MonoBehaviour {
+    public RotationPoll[] rotaters;
+
+    const float POLL_RATE = 1f;
+    RotationPoll active = null;
+    Coroutine coroutine;
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        for(int i = 0; i < rotaters.Length; i++)
+        {
+
+            if (Input.GetKeyDown("" + i))
+            {
+                Debug.Log("New Object Chosen");
+                setActive(rotaters[i]);
+                if (coroutine != null) StopCoroutine(coroutine);
+
+                coroutine=StartCoroutine("PollRotation");
+
+                continue;          
+            }
+
+            if (rotaters[i] != active)
+            {
+                rotaters[i].active = false;
+            }
+      
+        }
+	}
+
+    void setActive(RotationPoll toBeActive)
+    {
+        active = toBeActive;
+        toBeActive.active = true;
+        Debug.Log(active.active);
+    }
+
+    IEnumerator PollRotation()
+    {
+        while (active != null)
+        {
+            Debug.Log("Rotation: " + active.transform.eulerAngles);
+            yield return new WaitForSeconds(POLL_RATE);   
+        }      
+    }
+}
---
+++ Assets/RotationPoll.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class RotationPoll : MonoBehaviour {
+    public bool active = false;
+
+    public enum AXIS {
+        X=0,
+        Y=1,
+        Z=2
+    }
+
+
+    const int OSCILLATION_ANGLE = 80;
+    const int OSCILLATION_SPEED = 4;
+    const int PERLIN_NOISE = 4;
+
+
+    float currDeltaRotation;
+    float currRotation;
+
+    float left;
+    float right;
+	// Use this for initialization
+	void Start () {
+        left = 360 - OSCILLATION_ANGLE;
+        right = OSCILLATION_ANGLE;
+	}
+	// Update is called once per frame
+	void Update () {
+        if (!active) return;
+    }
+
+   
+
+    public void rotate(AXIS axis)
+    {
+        currDeltaRotation = Time.time * OSCILLATION_SPEED;
+        float t = Mathf.Sin(currDeltaRotation) * OSCILLATION_ANGLE;
+        if (t < 0) t = 360 + t;
+        
+        Vector3 currRotation = transform.eulerAngles;
+        switch (axis)
+        {
+            case AXIS.X:
+                currRotation.x = t;
+                break;
+            case AXIS.Y:
+                currRotation.y = t;
+                break;
+            case AXIS.Z:
+                currRotation.z = t;
+                break;
+        }
+        transform.eulerAngles = currRotation;
+    }
+
+    public void perlinNoise(AXIS axis)
+    {
+
+        float s = Mathf.PerlinNoise(transform.eulerAngles.x / 360, transform.eulerAngles.y);
+        s -= .5f;
+        s *= PERLIN_NOISE;
+        Vector3 currRotation = transform.eulerAngles;
+        switch (axis)
+        {
+            case AXIS.X:
+                currRotation.x = s;
+                break;
+            case AXIS.Y:
+                currRotation.y = s;
+                break;
+            case AXIS.Z:
+                currRotation.z = s;
+                break;
+        }
+        transform.eulerAngles = currRotation;
+
+    }
+}
